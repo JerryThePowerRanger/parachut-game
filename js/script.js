e4827b6fe6aff807221105fe386dc2a660bca650
Jakob Bal <baljakob9@gmail.com>
2015-12-16 11:52:11 +0100

    Hinzufügen der Hoehe und Breite des Springers

--- js/script.js
+++ js/script.js
@@ -31,11 +31,10 @@ function start(){
         asset.scaleY = scale_y;
         asset.x = x;
         asset.y = y;
-        assets.push(asset);
-       /* var asset = new createjs.Shape();
-        asset.graphics
-		  .beginFill('red')
-		  .drawRect(x, y, 50, 50);*/
+        asset.asset_id = preload_id;
+        var bounds = parachuter.getTransformedBounds();
+        asset.height = bounds.height;
+        asset.width = bounds.width;
         assets.push(asset);
         stage.addChild(asset);
         stage.update();
@@ -48,6 +47,9 @@ function start(){
         asset.x = x;
         asset.y = y;
         parachuter = asset;
+        var bounds = parachuter.getTransformedBounds();
+        parachuter.height = bounds.height;
+        parachuter.width = bounds.width;
         stage.addChild(asset);
         stage.update();
         
@@ -66,54 +68,90 @@ function start(){
         };
     }
     
+    function getAsset(asset_id){
+        for(var i = 0; i < assets.length; i++){
+            if(assets[i].asset_id === asset_id){
+                return assets[i];
+            }
+        }
+    }
+    
     function moveParachuter(target_x, target_y){
         var vector_times = calculateVector(movement_x, movement_y);
+        var landing_plattform = getAsset('plattform');
         if(parachuter.x < 800){
             parachuter.x += vector_times.time_x + delta_movement_x;
         }
         
-        if(parachuter.y < 500){
+        if(parachuter.y < (landing_plattform.y - landing_plattform.height)){
             parachuter.y += vector_times.time_y + delta_movement_y;
+        }
+        
+        if(parachuter.y === (landing_plattform.y - landing_plattform.height)) 
+            && parachuter.x === (landing_plattform.x)){
+            console.log('wuhu');  
+        }
+        
+        
+    }
+    
+    function checkMovement(mvmt, mvmt_number){
+        if(mvmt_number > 0){
+            if(mvmt < mvmt_number){
+                return (mvmt + (mvmt_number / 10));
+            }else{
+                return mvmt_number;
+            }
+        }else{
+            if(mvmt > mvmt_number){
+                return (mvmt + (mvmt_number / 10));
+            }else{
+                return mvmt_number;
+            }
         }
     }
     
     function movementParachuter(e){
         switch(e.keyCode){
             case 37:
-                delta_movement_x = -0.2;
+                delta_movement_x = checkMovement(delta_movement_x, -0.2);
             break;
             case 38:
-                delta_movement_y = -0.1;
+                delta_movement_y = checkMovement(delta_movement_y, -0.1);
             break;
             case 39:
-                delta_movement_x = 0.2;
+                delta_movement_x = checkMovement(delta_movement_x, 0.2);
             break;
             case 40:
-                delta_movement_y = 0.1;
+                delta_movement_y = checkMovement(delta_movement_y, 0.1);
             break;
         }
     }
     
+    /*
+    * Initialisierungsfunktion des Spiels
+    */
     function init(){
         stage = new createjs.Stage(document.getElementById('canvas'));
-                drawParachuter(20,20,0.2,0.2);
-
-        
+        drawParachuter(20,20,0.2,0.2);
         drawAsset(50,100,0.5,0.5,'cloud_1');
         drawAsset(200,100,0.5,0.5,'cloud_2');
         drawAsset(300,200,0.2,0.2,'thunder_cloud');
         drawAsset(100,200,0.3,0.3,'coin');
         drawAsset(300,300,0.3,0.3,'coin');
         drawAsset(500,300,0.3,0.3,'coin');
-        drawAsset(600,800,0.5,0.5,'plattform');
+        drawAsset(700,540,0.3,0.3,'plattform');
         
        // var x = createTicker
         
         for(var i = 0; i < assets.length; i++){
             var time = 10000 * (Math.random() + 1);
-            createjs.Tween.get(assets[i], {loop: true})
-                .to({x:500}, time, createjs.Ease.getPowInOut(2))
-                .to({x:0}, time, createjs.Ease.getPowInOut(2));
+            if(assets[i].asset_id !== 'plattform'){
+                createjs.Tween.get(assets[i], {loop: true})
+                    .to({x:500}, time, createjs.Ease.getPowInOut(2))
+                    .to({x:0}, time, createjs.Ease.getPowInOut(2));
+            }
+
             
         }
         
